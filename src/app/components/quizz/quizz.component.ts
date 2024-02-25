29889ebd61107fe2ace83b7417ac6b4e4a66bdfa
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2024-02-25 09:37:00 -0300

    * Criação de botão para recomeçar o questionário. * Implementação de ajustes de responsividade.

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -5,7 +5,10 @@ import quizz_questions from '../../../assets/data/quizz_questions.json';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: [
+		'./quizz.component.css',
+		'./quizz.component.resp.css'
+	]
 })
 export class QuizzComponent implements OnInit {
 
